19434a01fb5e31e97432b1b364e5b45018b740b5
kami2 <kamiviolet@gmail.com>
2023-09-16 13:57:51 +0100

    Add project files.

---
+++ Models/DatabaseSettingModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BookStoreApi.Models
+{
+    public class DatabaseSettingModel
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string BookCollectionName { get; set; } = null!;
+
+    }
+}
---
+++ Services/BooksService.cs
@@ -0,0 +1,36 @@
+﻿using BookStoreApi.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace BookStoreApi.Services
+{
+    public class BooksService
+    {
+        public readonly IMongoCollection<BookModel> _bookCollection;
+        public BooksService(IOptions<DatabaseSettingModel> bookStoreDatabaseSetting)
+        {
+            var mongoClient = new MongoClient(bookStoreDatabaseSetting.Value.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(bookStoreDatabaseSetting.Value.DatabaseName);
+
+            _bookCollection = mongoDatabase.GetCollection<BookModel>(
+                bookStoreDatabaseSetting.Value.BookCollectionName);
+        }
+
+        public async Task<List<BookModel>> GetAsync() =>
+            await _bookCollection.Find(_ => true).ToListAsync();
+
+        public async Task<BookModel?> GetAsync(string id) =>
+            await _bookCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(BookModel newBook) =>
+            await _bookCollection.InsertOneAsync(newBook);
+
+        public async Task UpdateAsync(string id, BookModel updatedBook) =>
+            await _bookCollection.ReplaceOneAsync(x => x.Id == id, updatedBook);
+
+        public async Task RemoveAsync(string id) =>
+            await _bookCollection.DeleteOneAsync(x => x.Id == id);
+
+    }
+}
